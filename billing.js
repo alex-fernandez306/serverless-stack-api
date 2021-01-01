d2be37cc24709f7c228ebd3dbdf6ee3a0d829a56
Alex Fernandez <63-alex.fernandez@users.noreply.gitlab.whiteskylabs.com>
2021-01-01 11:46:22 +1100

    Add munit test to the serverless api

---
+++ billing.js
@@ -0,0 +1,16 @@
+import stripePackage from "stripe";
+import calculateCost from "./libs/billing-lib";
+import handler from "./libs/handler-lib";
+export const main = handler(async (event, context) => {
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
+  const stripe = stripePackage(process.env.stripeSecretKey);
+  await stripe.charges.create({
+    source,
+    amount,
+    description,
+    currency: "usd",
+  });
+  return { status: true };
+});
