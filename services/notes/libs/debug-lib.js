de7c923390bab73b9d11d948c2b881373ecb9085
Alex Fernandez <63-alex.fernandez@users.noreply.gitlab.whiteskylabs.com>
2021-01-03 11:19:06 +1100

    Adding error logging

---
+++ services/notes/libs/debug-lib.js
@@ -0,0 +1,35 @@
+import util from "util";
+import AWS from "aws-sdk";
+import { debug } from "console";
+
+let logs;
+
+AWS.config.logger = { log: debug };
+
+const debug = () => {
+  logs.push({
+    date: new Date(),
+    string: util.format.apply(null, arguments),
+  });
+}
+
+const init = (event, context) => {
+  logs = [];
+  debug("API Event", {
+    body: event.body,
+    pathParameters: event.pathParameters,
+    queryStringParameters: event.queryStringParameters,
+  });
+}
+
+const flush = (e) => {
+  logs.forEach(({date, string}) => console.debug(date, string));
+  console.error(e);
+}
+
+export default {
+  debug,
+  flush,
+  init,
+}
+
